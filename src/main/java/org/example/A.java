917aca7e5e448111526a4a20f9044455215c82d7
Xiaorui  Wang <xiaorui.wang@usi.ch>
2024-10-08 19:41:56 +0200

    drop some unrelated class

--- src/main/java/org/example/A.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class A {
-    String getName() { return " I ’m A " ; }
-
-    void printSomething(A aParam ) {
-        System.out.println( " B at A : " + aParam.getName() );
-     }
-}
